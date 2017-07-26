9ecf66c1f15460574cbbe2e2089275ec3c2fb0e5
spialelo <ndphillips@gmail.com>
2017-07-26 00:29:22 -0400

    Create to do method built

--- src/components/app.js
+++ src/components/app.js
@@ -1,47 +1,59 @@
-import React from 'react';
-import CreateTodo from './create-todo';
-import TodosList from './todos-list';
-
+import React from "react";
+import CreateTodo from "./create-todo";
+import TodosList from "./todos-list";
 
 const todos = [
   {
-    task:'make React tutorial',
+    task: "make React tutorial",
     isCompleted: false
   },
-    {
-    task:'eat dinner',
+  {
+    task: "eat dinner",
     isCompleted: true
   },
-    {
-    task:'review React and Webpack',
+  {
+    task: "review React and Webpack",
     isCompleted: true
   }
 ];
 
-
 class App extends React.Component {
-  
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       todos
-    }
+    };
   }
-  
-  
-  
-  
+
+  createTask(task) {
+    {
+      /*tutorial way
+    
+    this.state.todos.push({
+    task,
+    isCompleted: false
+    });
+    this.setState({todos: this.state.todos});
+     */
+    }
+
+    const currToDos = { ...this.state.todos };
+    this.state.todos.push({
+      task,
+      isCompleted: false
+    });
+    this.setState({ todos: this.state.todos });
+  }
+
   render() {
     return (
       <div>
-      <h2>Rendering from App.</h2>
-       <CreateTodo /> 
-      <TodosList todos = {this.state.todos} />
-      </div>  
+        <h2>Rendering from App.</h2>
+        <CreateTodo createTask={this.createTask.bind(this)} />
+        <TodosList todos={this.state.todos} />
+      </div>
     );
   }
-  
-  
 }
 
 export default App;
--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 class CreateTodo extends React.Component {
+  handleCreate(event) {
+    event.preventDefault();
+    this.props.createTask(this.refs.createInput.value);
+    this.refs.createInput.value = "";
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" placeholder="What do I need to do" />
+      <form onSubmit={this.handleCreate.bind(this)}>
+        <input
+          type="text"
+          placeholder="What do I need to do"
+          ref="createInput"
+        />
         <button>Create</button>
       </form>
     );
